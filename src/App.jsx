4ac976f44cf0f310d907fb7ca7320587f1ac7014
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-07-14 17:59:35 +0100

    homepagefix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
-// import Landing from "./pages/Landing";
-// import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./Pages/Landing";
-import Home from "./Pages/Home";
+import LandingPage from "./Pages/LandingPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Routes path="/" element={LandingPage}/>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </>
   );
